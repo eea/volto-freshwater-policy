78318e3b55d31c560bb097899c583fa076c265b6
laszlocseh <csehlaszlo91@gmail.com>
2023-10-03 09:31:12 +0300

    Remove some comments

--- src/components/Blocks/CaseStudyExplorer/utils.test.js
+++ src/components/Blocks/CaseStudyExplorer/utils.test.js
@@ -1,7 +1,5 @@
 import './mockJsdom';
-// import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-// import { render } from '@testing-library/react';
 import { filterCases } from './utils';
 
 test('filterCases', () => {
