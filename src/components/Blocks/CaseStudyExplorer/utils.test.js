bdebd5a1284592bfabe917f90c296ec20c2d0efd
laszlocseh <csehlaszlo91@gmail.com>
2023-10-03 11:16:22 +0300

    Add tests

--- src/components/Blocks/CaseStudyExplorer/utils.test.js
+++ src/components/Blocks/CaseStudyExplorer/utils.test.js
@@ -1,8 +1,16 @@
 import './mockJsdom';
 import '@testing-library/jest-dom/extend-expect';
-import { filterCases } from './utils';
+import { filterCases, getFilters } from './utils';
 
 test('filterCases', () => {
   const mockCases = filterCases([], [], [], '');
   expect(mockCases).toStrictEqual([]);
+});
+
+test('getFilters', () => {
+  const mockFilters = getFilters([]);
+  expect(mockFilters).toStrictEqual({
+    nwrms_implemented: {},
+    sectors: {},
+  });
 });
