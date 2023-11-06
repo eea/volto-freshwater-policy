eafdc11e76b09b65ae004bef34651d45a0479add
kreafox <krisztina.n.elekes@gmail.com>
2023-11-06 10:47:28 +0200

    Remove modal for tableau visualization

--- src/components/theme/MetadataListingView/MetadataHeader.jsx
+++ src/components/theme/MetadataListingView/MetadataHeader.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Modal } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import {
   ItemMetadata,
   ItemTitle,
@@ -10,6 +11,8 @@ import { useLocation, useHistory } from 'react-router-dom';
 
 const MetadataHeader = (props) => {
   const { item } = props;
+  const url = item.source ? item.source[0]['@id'] : item['@id'];
+  const isTableauVisualization = item['@type'] === 'visualization_tableau';
   const [isOpenModal, setOpenModal] = useState(false);
   const history = useHistory();
   const location = useLocation();
@@ -27,38 +30,46 @@ const MetadataHeader = (props) => {
 
   return (
     <>
-      <div
-        className="listing-title"
-        onClick={() => {
-          setOpenModal(true);
-          history.push({
-            hash: item?.id,
-          });
-        }}
-        onKeyDown={() => setOpenModal(true)}
-        role="button"
-        tabIndex="0"
-      >
-        <h3>{item.title ? item.title : item.id}</h3>
-      </div>
+      {isTableauVisualization ? (
+        <h3>
+          <Link to={url}>{item.title}</Link>
+        </h3>
+      ) : (
+        <>
+          <div
+            className="listing-title"
+            onClick={() => {
+              setOpenModal(true);
+              history.push({
+                hash: item?.id,
+              });
+            }}
+            onKeyDown={() => setOpenModal(true)}
+            role="button"
+            tabIndex="0"
+          >
+            <h3>{item.title ? item.title : item.id}</h3>
+          </div>
 
-      <Modal
-        className="item-metadata-modal"
-        open={isOpenModal}
-        onClose={closeModal}
-        size="large"
-        closeIcon
-        centered
-      >
-        <Modal.Header>
-          <ItemMetadataSnippet item={item} />
-          <ItemTitle item={item} />
-        </Modal.Header>
+          <Modal
+            className="item-metadata-modal"
+            open={isOpenModal}
+            onClose={closeModal}
+            size="large"
+            closeIcon
+            centered
+          >
+            <Modal.Header>
+              <ItemMetadataSnippet item={item} />
+              <ItemTitle item={item} />
+            </Modal.Header>
 
-        <Modal.Content>
-          <ItemMetadata item={item} map_preview={true} shareItem={true} />
-        </Modal.Content>
-      </Modal>
+            <Modal.Content>
+              <ItemMetadata item={item} map_preview={true} shareItem={true} />
+            </Modal.Content>
+          </Modal>
+        </>
+      )}
     </>
   );
 };
