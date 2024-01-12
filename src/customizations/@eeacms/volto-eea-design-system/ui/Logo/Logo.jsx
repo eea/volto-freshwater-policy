b5431228331afefcf21c011e682d4d698ef8dc76
nileshgulia1 <nileshgulia@gmail.com>
2024-01-12 13:50:23 +0530

    breadcrumbs and contentBreadcrumbs custmoizations

--- src/customizations/@eeacms/volto-eea-design-system/ui/Logo/Logo.jsx
+++ src/customizations/@eeacms/volto-eea-design-system/ui/Logo/Logo.jsx
@@ -6,7 +6,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { useSelector } from 'react-redux';
+import config from '@plone/volto/registry';
 
 /**
  * Logo component class.
@@ -15,10 +15,10 @@ import { useSelector } from 'react-redux';
  * @returns {string} Markup of the component.
  */
 const Logo = ({ src, invertedSrc, id, url, alt, title, inverted }) => {
-  const root = useSelector((state) => state.breadcrumbs.root);
+  const href = config.settings.prefixPath ?? '/';
 
   return (
-    <Link to={root} title={title} className={'logo'}>
+    <Link to={href} title={title} className={'logo'}>
       <LazyLoadImage
         src={inverted ? invertedSrc : src}
         alt={alt}
