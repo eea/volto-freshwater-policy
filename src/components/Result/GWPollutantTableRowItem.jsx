e50d9f9b69f03cee8d7783a49eee97a17946315f
laszlocseh <csehlaszlo91@gmail.com>
2024-04-12 09:22:35 +0300

    code cleanup

--- src/components/Result/GWPollutantTableRowItem.jsx
+++ src/components/Result/GWPollutantTableRowItem.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useAppConfig } from '@eeacms/search/lib/hocs';
 import { Table, Label } from 'semantic-ui-react';
-// import { ResultHeader } from '@eeacms/search/components/Result/ResultModal';
 
 const normalizeStr = (str) => {
   if (typeof str === 'number') {
@@ -20,7 +19,6 @@ const WrappedRowItem = (props) => {
   const { result } = props;
   const days =
     (Date.now() - Date.parse(result['issued']?.raw)) / 1000 / 60 / 60 / 24;
-  // console.log('card props', props, appConfig);
   let expired = false;
   if (result['expires']?.raw !== undefined) {
     expired = Date.parse(result['expires']?.raw) < Date.now();
