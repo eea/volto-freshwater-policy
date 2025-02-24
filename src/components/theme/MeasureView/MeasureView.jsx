b7745cd78dd30285213de637f1b9ff05c28da72d
laszlocseh <csehlaszlo91@gmail.com>
2025-02-24 13:38:27 +0200

    fix: sonarqube bug

--- src/components/theme/MeasureView/MeasureView.jsx
+++ src/components/theme/MeasureView/MeasureView.jsx
@@ -139,7 +139,15 @@ const ImageSource = (props) => {
 
 const renderCircles = (effect) => {
   const circleCount =
-    effect === 'High' ? 3 : effect === 'Medium' ? 2 : effect === 'Low' ? 1 : 1;
+    effect === 'High'
+      ? 3
+      : effect === 'Medium'
+      ? 2
+      : effect === 'Low'
+      ? 1
+      : effect === 'Negative'
+      ? 1
+      : 0;
   const circleClass = effect; // 'high', 'medium', or 'low'
 
   return (
