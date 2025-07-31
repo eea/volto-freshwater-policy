b5d3131cd9f5c3fc9b056bb447cf99e190e0671a
vladcalin-edw <calin.vlad@eaudeweb.ro>
2025-07-31 19:55:00 +0300

    Added pagination

--- src/components/controlpanel/VisualizationUssage.jsx
+++ src/components/controlpanel/VisualizationUssage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useCallback, useMemo } from 'react';
 import {
   Button,
   Checkbox,
@@ -13,7 +13,7 @@ import {
   Segment,
   Table,
 } from 'semantic-ui-react';
-import { connect, useSelector } from 'react-redux';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import { compose } from 'redux';
 import { Link } from 'react-router-dom';
 import { createPortal } from 'react-dom';
@@ -40,6 +40,7 @@ import backSVG from '@plone/volto/icons/back.svg';
 import { getVisualizationUssage } from '@eeacms/volto-freshwater-policy/actions/visualizationUssage';
 import Circle from '@plone/volto/components/manage/Contents/circle';
 import config from '@plone/volto/registry';
+import map from 'lodash/map';
 
 const messages = defineMessages({
   private: {
@@ -72,9 +73,45 @@ const messages = defineMessages({
   },
 });
 
+const itemsPerPageChoices = [10, 25, 50, 'All'];
+
 function VisualizationStatus(props) {
   const intl = useIntl();
   const visualizations = useSelector((state) => state.visualizationUssage);
+  const dispatch = useDispatch();
+
+  const [filterQuery, setFilterQuery] = useState('');
+  const [activePage, setActivePage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+
+  const updateResults = useCallback(() => {
+    const options = {
+      query: filterQuery,
+      batchStart: (activePage - 1) * itemsPerPage,
+      batchSize: itemsPerPage === 'All' ? 999999999999 : itemsPerPage,
+    };
+    dispatch(getVisualizationUssage(getBaseUrl(props.pathname), options));
+  }, [
+      activePage,
+      dispatch,
+      filterQuery,
+      itemsPerPage,
+      props.pathname,
+    ]);
+
+  // Calculate page count from results
+  const pages = useMemo(() => {
+    let pages = Math.ceil(visualizations.items_total / itemsPerPage);
+    if (pages === 0 || isNaN(pages)) {
+      pages = '';
+    }
+    return pages;
+  }, [visualizations.items_total, itemsPerPage]);
+
+  // Update results after changing the page.
+  // (We intentionally leave updateResults out of the deps.)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => updateResults(), [activePage, itemsPerPage]);
 
   useEffect(() => {
     props.getVisualizationUssage(getBaseUrl(props.pathname), {
@@ -110,10 +147,10 @@ function VisualizationStatus(props) {
           <Segment>
             <Table cell>
               <Table.Row>
-                <Table.HeaderCell width="1">
+                <Table.HeaderCell>
                   <FormattedMessage id="Name" defaultMessage="Name" />
                 </Table.HeaderCell>
-                <Table.HeaderCell width="10">
+                <Table.HeaderCell>
                   <FormattedMessage id="Ussage" defaultMessage="Ussage" />
                 </Table.HeaderCell>
               </Table.Row>
@@ -141,8 +178,8 @@ function VisualizationStatus(props) {
                               <Circle
                                 color={
                                   config.settings.workflowMapping[
-                                    obj.review_state
-                                  ].color
+                                  obj.review_state
+                                ].color
                                 }
                                 size="15px"
                               />
@@ -168,6 +205,60 @@ function VisualizationStatus(props) {
                 ))}
               </Table.Body>
             </Table>
+
+            <div
+              style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                marginBottom: '20px',
+              }}
+            >
+
+              {pages && (
+                <Pagination
+                  boundaryRange={0}
+                  activePage={activePage}
+                  ellipsisItem={null}
+                  firstItem={null}
+                  lastItem={null}
+                  siblingRange={1}
+                  totalPages={pages}
+                  onPageChange={(e, { activePage }) =>
+                    setActivePage(activePage)
+                  }
+                />
+              )}
+
+              <Menu.Menu
+                position="right"
+                style={{ display: 'flex', marginLeft: 'auto' }}
+              >
+                <Menu.Item style={{ color: 'grey' }}>
+                  <FormattedMessage id="Show" defaultMessage="Show" />:
+                </Menu.Item>
+                {map(itemsPerPageChoices, (size) => (
+                  <Menu.Item
+                    style={{
+                      padding: '0 0.4em',
+                      margin: '0em 0.357em',
+                      cursor: 'pointer',
+                    }}
+                    key={size}
+                    value={size}
+                    active={size === itemsPerPage}
+                    onClick={(e, { value }) => {
+                      setItemsPerPage(value);
+                      setActivePage(1);
+                    }}
+                  >
+                    {size}
+                  </Menu.Item>
+                ))}
+              </Menu.Menu>
+
+            </div>
+
           </Segment>
         </Segment.Group>
       </article>
